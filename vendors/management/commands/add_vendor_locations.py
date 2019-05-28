f8b437184651a221226404d708b31a4bf9a69327
Ed Rivas <ed@unplug.studio>
2019-05-28 17:51:03 -0600

    feat: don't re-process vendors with wrong addresses
    This way we don't hit the Maps API constantly

--- vendors/management/commands/add_vendor_locations.py
+++ vendors/management/commands/add_vendor_locations.py
@@ -1,11 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-import googlemaps
-
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from vendors.models import Vendor
+from vendors.models import Vendor, GeolocationError
 
 
 class Command(BaseCommand):
@@ -14,26 +11,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         processed = 0
         failed = 0
-        api = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
 
         for vendor in Vendor.objects.filter(geolocation=""):
             self.stdout.write("Processing {}".format(vendor))
             try:
-                result = api.geocode(vendor.address)
-            except googlemaps.exceptions.ApiError as e:
+                vendor.update_geolocation()
+            except GeolocationError as e:
                 failed += 1
-                self.stderr.write("API error ({}) when processing {}".format(e, vendor))
-                continue
-
-            if len(result) == 0:
-                failed += 1
-                self.stderr.write("Error: No geolocation results found for {}".format(vendor))
-                continue
-
-            vendor.geolocation.lat = result[0]["geometry"]["location"]["lat"]
-            vendor.geolocation.lon = result[0]["geometry"]["location"]["lng"]
-            vendor.save()
-            processed += 1
+                self.stderr.write(str(e))
+            else:
+                processed += 1
 
         self.stdout.write("Processed: {}".format(processed))
         self.stdout.write("Failed: {}".format(failed))
--- vendors/models.py
+++ vendors/models.py
@@ -1,11 +1,21 @@
 from __future__ import absolute_import, unicode_literals
 
+import googlemaps
+
 from django.db import models
 from django_google_maps import fields as map_fields
+from django.utils.timezone import now
 
+from mezzanine.conf import settings
 from mezzanine.core.models import TimeStamped
 
 from mezzy.utils.models import Titled
+
+
+class GeolocationError(Exception):
+    """
+    Indicates a problem when performing geolocation queries for a vendor.
+    """
 
 
 class Subject(Titled):
@@ -26,6 +36,7 @@ class Vendor(TimeStamped, Titled):
     """
     address = map_fields.AddressField(max_length=200)
     geolocation = map_fields.GeoLocationField(max_length=100)
+    geolocation_updated = models.DateTimeField(blank=True, null=True)
 
     phone = models.CharField("Phone", max_length=100)
     email = models.EmailField("Email", max_length=254)
@@ -37,3 +48,30 @@ class Vendor(TimeStamped, Titled):
 
     class Meta:
         ordering = ["title"]
+
+    def update_geolocation(self, force=False):
+        """
+        Attempt to get new coordinates based on a vendor's address.
+        """
+        # Skip vendors with a failed attempt that haven't been updated since
+        if not force:
+            if self.geolocation_updated and self.geolocation_updated >= self.updated:
+                raise GeolocationError(
+                    "Vendor {} has not been updated since last geocode attempt".format(self))
+
+        error = None
+        api = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+        try:
+            result = api.geocode(self.address)
+            self.geolocation.lat = result[0]["geometry"]["location"]["lat"]
+            self.geolocation.lon = result[0]["geometry"]["location"]["lng"]
+        except googlemaps.exceptions.ApiError as e:
+            error = "API error ({}) when processing {}".format(e, self)
+        except IndexError:
+            error = "No geolocation results found for {}".format(self)
+
+        self.geolocation_updated = now()
+        self.save()
+
+        if error:
+            raise GeolocationError(error)
